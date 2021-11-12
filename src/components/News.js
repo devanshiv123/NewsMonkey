54919a8b8b328eb9582a2c5135bce01f65ec9a79
devanshiv123 <devanshiv123@gmail.com>
2021-11-12 21:29:09 +0530

    changes in api

--- src/components/News.js
+++ src/components/News.js
@@ -16,7 +16,7 @@ const News=(props)=>{
     
     const updateNews=async()=>{
         props.setProgress(10)
-        const url=`https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`
+        const url=`https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=1bd4a4bca32143f4952c53d2ad96c036&page=${page}&pageSize=${props.pageSize}`
         setLoading(true)
         let data=await fetch(url)
         props.setProgress(30)
